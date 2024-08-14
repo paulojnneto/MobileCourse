f8661bb8a0e4ec618971a6539a796643b624c5af
Paulo José Neves Nto <paulo.jose.n@outlook.com>
2024-08-14 17:06:44 -0300

    Added logic to timer

--- main.gd
+++ main.gd
@@ -1,8 +1,16 @@
 extends Control
 
 var score : int = 0
+var time_left : int = 30
 
+func _ready():
+	$TimeLeftLabel.text = "TIME LEFT: " + str(time_left) + " s"
+	
 func _on_increase_score_button_pressed():
 	score += 1
 	$ScoreLabel.text = "SCORE: " + str(score)
 	Input.vibrate_handheld(125)
+
+func _on_time_left_timer_timeout():
+	time_left -= 1
+	$TimeLeftLabel.text = "TIME LEFT: " + str(time_left) + " s"
